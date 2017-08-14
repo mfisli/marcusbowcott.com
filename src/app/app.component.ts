313f7bc9832666dda4f837bc3e01be83104fcdd2
Maks Fisli <mfisli2@gmail.com>
2017-08-13 21:21:56 -0700

    Bootstrap got funny so i'm just going to do it through script tags. Centered images and change fonts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  title = 'Marcus Bowcott';
+  public  title:   string = 'Marcus Bowcott';
+  private baseUrl: string = "http://marcusbowcottapi.azurewebsites.net/";
+  private apiUrl:  string = this.baseUrl + "api/artworksAPI";
+  public  data:    any    = [{Name:"Loading ..."}];
+
+  constructor(private http: Http) {
+    console.log('AppComponent Constructor');
+    this.getData();
+  }
+  getJSON(): any {
+    console.log("Getting data ...");
+    console.log(typeof this.http.get(this.apiUrl).map(res => res.json()));
+    return this.http.get(this.apiUrl).map(res => res.json());
+  }
+  getData(): void {
+    this.getJSON().subscribe(data => {
+      console.log("__data__");
+      console.log(data);
+      this.data = data;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
